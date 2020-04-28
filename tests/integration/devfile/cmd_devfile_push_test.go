976ed8917e2cde0b18f278bdc93f44a04ebe764b
Jonathan West <jgwest@ca.ibm.com>
2020-04-28 03:57:09 -0400

    Add machine readable event logging support

--- tests/integration/devfile/cmd_devfile_push_test.go
+++ tests/integration/devfile/cmd_devfile_push_test.go
@@ -1,15 +1,11 @@
 package devfile
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/openshift/odo/pkg/machineoutput"
 	"github.com/openshift/odo/tests/helper"
 	"github.com/openshift/odo/tests/integration/devfile/utils"
 
@@ -102,12 +98,12 @@ var _ = Describe("odo devfile push command tests", func() {
 
 			output := helper.CmdShouldPass("odo", "push", "-o", "json", "--devfile", "devfile.yaml", "--namespace", namespace)
 
-			analyzePushConsoleOutput(output)
+			utils.AnalyzePushConsoleOutput(output)
 
 			// update devfile and push again
 			helper.ReplaceString("devfile.yaml", "name: FOO", "name: BAR")
 			output = helper.CmdShouldPass("odo", "push", "-o", "json", "--devfile", "devfile.yaml", "--namespace", namespace)
-			analyzePushConsoleOutput(output)
+			utils.AnalyzePushConsoleOutput(output)
 
 		})
 
@@ -290,116 +286,3 @@ var _ = Describe("odo devfile push command tests", func() {
 	})
 
 })
-
-// Analyze the output of 'odo push -o json' for the machine readable event push test above.
-func analyzePushConsoleOutput(pushConsoleOutput string) {
-
-	lines := strings.Split(strings.Replace(pushConsoleOutput, "\r\n", "\n", -1), "\n")
-
-	var entries []machineoutput.MachineEventLogEntry
-
-	// Ensure that all lines can be correctly parsed into their expected JSON structure
-	for _, line := range lines {
-
-		if len(strings.TrimSpace(line)) == 0 {
-			continue
-		}
-
-		fmt.Println("Processing output line: " + line)
-
-		lineWrapper := machineoutput.MachineEventWrapper{}
-
-		err := json.Unmarshal([]byte(line), &lineWrapper)
-		Expect(err).NotTo(HaveOccurred())
-
-		entry, err := lineWrapper.GetEntry()
-		Expect(err).NotTo(HaveOccurred())
-
-		entries = append(entries, entry)
-
-	}
-
-	if len(entries) < 4 {
-		Fail("Expected at least 4 entries, corresponding to command/action execution.")
-	}
-
-	// Timestamps should be monotonically increasing
-	mostRecentTimestamp := float64(-1)
-	for _, entry := range entries {
-		timestamp, err := strconv.ParseFloat(entry.GetTimestamp(), 64)
-		Expect(err).NotTo(HaveOccurred())
-
-		if timestamp < mostRecentTimestamp {
-			Fail("Timestamp was not monotonically increasing " + entry.GetTimestamp() + " " + strconv.FormatFloat(mostRecentTimestamp, 'E', -1, 64))
-		}
-
-		mostRecentTimestamp = timestamp
-	}
-
-	// First look for the expected devbuild events, then look for the expected devrun events.
-	expectedEventOrder := []struct {
-		entryType   machineoutput.MachineEventLogEntryType
-		commandName string
-	}{
-		// first the devbuild command (and its action) should run
-		{
-			machineoutput.TypeDevFileCommandExecutionBegin,
-			"devbuild",
-		},
-		{
-			machineoutput.TypeDevFileActionExecutionBegin,
-			"devbuild",
-		},
-		{
-			// at least one logged line of text
-			machineoutput.TypeLogText,
-			"",
-		},
-		{
-			machineoutput.TypeDevFileActionExecutionComplete,
-			"devbuild",
-		},
-		{
-			machineoutput.TypeDevFileCommandExecutionComplete,
-			"devbuild",
-		},
-		// next the devbuild command (and its action) should run
-		{
-			machineoutput.TypeDevFileCommandExecutionBegin,
-			"devrun",
-		},
-		{
-			machineoutput.TypeDevFileActionExecutionBegin,
-			"devrun",
-		},
-		{
-			// at least one logged line of text
-			machineoutput.TypeLogText,
-			"",
-		},
-		{
-			machineoutput.TypeDevFileActionExecutionComplete,
-			"devrun",
-		},
-		{
-			machineoutput.TypeDevFileCommandExecutionComplete,
-			"devrun",
-		},
-	}
-
-	currIndex := -1
-	for _, nextEventOrder := range expectedEventOrder {
-		entry, newIndex := machineoutput.FindNextEntryByType(currIndex, nextEventOrder.entryType, entries)
-		Expect(entry).NotTo(BeNil())
-		Expect(newIndex).To(BeNumerically(">=", 0))
-		Expect(newIndex).To(BeNumerically(">", currIndex)) // monotonically increasing index
-
-		// We should see devbuild for the first set of events, then devrun
-		commandName := machineoutput.GetCommandName(entry)
-		Expect(commandName).To(Equal(nextEventOrder.commandName))
-
-		currIndex = newIndex
-		fmt.Printf("Index: %d\n", currIndex)
-	}
-
-}
